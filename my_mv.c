2e64aadc057425842ba74e10a91dcc5b3303b3b8
Cerve10 <146700482+Cerve10@users.noreply.github.com>
2023-12-16 17:58:36 +0100

    Update my_mv.c

--- my_mv.c
+++ my_mv.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    
-    if (argc != 3) {
-        
+void main(){
+char arxivo[999999999]
+char nombre_nuevo[9999999999]
+int fd,desc;
+printf("Introdueix el nom de l'arxiu: ");
+scanf("%s",arxivo);
+printf("Introdueix el nou nom de l'arxiu: ");
+scanf("%s",nombre_nuevo);
+if ((fd=open(arxivo,O_RDWR)) == -1){
+printf("Hi ha hagut un error a l'hora d'obrir l'arxiu \n");
+}
+else {
+desc = open(nombre_nuevo, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU );
+if (desc == -1){
+printf("Error creant el segon arxiu \n");
+}
+else{
+char buf[9999999999];
+ssize_t read(fd, buf, 9999999999);
+write(desc, buf, 9999999999);
+unlink(arxivo);
+}
+close(fd);
+close(desc);
+}
+}
