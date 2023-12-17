129a92c4a1dc6824084344f036c388d4e0e52b0f
Cerve10 <146700482+Cerve10@users.noreply.github.com>
2023-12-17 22:36:55 +0100

    Update my_mv.c

--- my_mv.c
+++ my_mv.c
@@ -4,30 +4,30 @@
 #include <unistd.h>
 
 void main(){
-char arxivo[999999999]
-char nombre_nuevo[9999999999]
-int fd,desc;
-printf("Introdueix el nom de l'arxiu: ");
-scanf("%s",arxivo);
-printf("Introdueix el nou nom de l'arxiu: ");
-scanf("%s",nombre_nuevo);
-if ((fd=open(arxivo,O_RDWR)) == -1){
-printf("Hi ha hagut un error a l'hora d'obrir l'arxiu \n");
-exit(1);
-}
-else {
-desc = open(nombre_nuevo, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU );
-if (desc == -1){
-printf("Error creant el segon arxiu \n");
-exit(1);
-}
-else{
-char buf[9999999999];
-ssize_t read(fd, buf, 9999999999);
-write(desc, buf, 9999999999);
-unlink(arxivo);
-}
-close(fd);
-close(desc);
+        char arxivo[999999999];
+        char nombre_nuevo[9999999999];
+        int fd,desc;
+        printf("Introdueix el nom de l'arxiu: ");
+        scanf("%s",arxivo);
+        printf("Introdueix el nou nom de l'arxiu: ");
+        scanf("%s",nombre_nuevo);
+        if ((fd=open(arxivo,O_RDWR)) == -1){
+                printf("Hi ha hagut un error a l'hora d'obrir l'arxiu \n");
+                exit(1);
+        }
+        else {
+                desc = open(nombre_nuevo, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU );
+        if (desc == -1){
+                printf("Error creant el segon arxiu \n");
+                exit(1);
+        }
+        else{
+                char buf[9999999999];
+                ssize_t lectura = read(fd, buf, 1000);
+                write(desc, buf, 1000);
+                unlink(arxivo);
+        }
+        close(fd);
+        close(desc);
 }
 }
